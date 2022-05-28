fbecac0ea12797b7c85e01de2b5e568efe1e9149
jmurphy5613 <jmurphy5613@gmail.com>
2022-05-28 00:41:15 -0400

    added nav links

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,7 +4,9 @@ import styles from './navbar.module.css';
 const Navbar = () => {
     return (
         <div className={styles.root}>
-            <img className={styles.logo} src="/logo.png"></img>
+            <img className={styles.logo} src="/logo.png"></img> 
+            <a href="" className={styles["nav-link"]}>Documentation</a>
+            <a href="" className={styles["nav-link"]}>Team</a>
         </div>
     )
 }
