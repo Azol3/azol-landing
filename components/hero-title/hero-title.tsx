44a547d2537103964e7a734aab0d00574e361652
jmurphy5613 <jmurphy5613@gmail.com>
2022-05-27 20:59:16 -0400

    added gradient

--- components/hero-title/hero-title.tsx
+++ components/hero-title/hero-title.tsx
@@ -4,8 +4,8 @@ import styles from './hero-title.module.css';
 const HeroTitle = () => {
     return (
         <div className={styles["title-container"]}>
-            <h1 className={styles["main-title"]}>The first decentralized reputation solution</h1>
-            <h3 className={styles.description}>Azol is on a mission to revolutionize the way that verification and authentication works on web3 dapps.</h3>
+            <h1 className={styles["main-title"]}>The first <span className={styles.gradient}>decentralized</span> reputation solution</h1>
+            <h3 className={styles.description}>Azol is on a mission to revolutionize the way that verification and authentication works on web3 dapps. Reduce the threat of botting and other bad actors by implementing a universal reputation system, so that your virtual identity follows you.</h3>
         </div>
     )
 }
