dd46938fe75997ea46164bfe8060863456b203c2
jmurphy5613 <jmurphy5613@gmail.com>
2022-05-29 22:57:58 -0400

    finally got the button

--- components/hero-title/hero-title.tsx
+++ components/hero-title/hero-title.tsx
@@ -12,8 +12,11 @@ const HeroTitle = () => {
             <h1 className={styles["main-title"]}>The first <span className={styles.gradient}>decentralized</span> reputation solution</h1>
             <h3 className={styles.description}>Azol is on a mission to revolutionize the way that verification and authentication works on web3. We want to implement a universal reputation system, so that your virtual identity follows you.</h3>
             <div className={styles["button-container"]}>
+                
                 <button onClick={e => router.push('https://discord.gg/azol')} className={styles["join-discord"]}>Join Discord</button>
-                <button onClick={e => router.push('https://azol.io/whitepaper.pdf')} className={styles["view-whitepaper"]}>Whitepaper</button>
+                {/* <button onClick={e => router.push('https://azol.io/whitepaper.pdf')} className={styles["view-whitepaper"]}>Whitepaper</button> */}
+                <button className={styles.btn}>Whitepaper</button>
+
             </div>
         </div>
     )
